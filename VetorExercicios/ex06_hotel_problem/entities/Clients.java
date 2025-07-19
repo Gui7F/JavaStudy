e092ea7194a1a68e215491791b097470e1999f77
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-07-19 05:19:00 +0000

    Add: problem hotel exercise

---
+++ VetorExercicios/ex06_hotel_problem/entities/Clients.java
@@ -0,0 +1,30 @@
+package VetorExercicios.ex06_hotel_problem.entities;
+
+public class Clients {
+    private String name;
+    private String email;
+    private int roomNumber;
+
+    public Clients() {
+    }
+
+    public Clients(String name, String email, int roomNumber){
+        this.name = name;
+        this.email = email;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoomNumber (){
+        return roomNumber;
+    }
+
+
+}
