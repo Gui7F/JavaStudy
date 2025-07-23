d0f90914604ebd7fb7130d9bea2ab46e9e87d091
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-07-23 05:09:39 +0000

    add: add the first matrix exercise for java_basics

--- java_basics/ex6_matrix/ex1_simple_exercise/src/application/App.java
+++ java_basics/ex6_matrix/ex1_simple_exercise/src/application/App.java
@@ -3,28 +3,47 @@
 package application;
 
 import java.util.Scanner;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter para uma matriz quadratica ou seja NxN: " );
-        int n = sc.nextInt();
-        sc.nextLine();
+        System.out.print("Digite o tamanho de N ou seja uma matriz NxN: " );
 
-        int [][] matrix = new int [n][n];
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
 
         //Vamos percorre preencher os dados da matrix por input
         System.out.println("Digite todos os numeros da matrix NxN: ");
         for(int i = 0 ; i < matrix.length ; i++ ){ //Percorre as linhas;
-           for(int j = 0 ; j < matrix[i].length ; i++){ //Percorre as colunas.
-                 int element = sc.nextInt();   
-                 matrix[i][j] = element; //Adiciona em cada posição                     
+           for(int j = 0 ; j < matrix[i].length ; j++){ //Percorre as colunas.  
+                 matrix[i][j] = sc.nextInt(); //Adiciona em cada posição                     
            }
         }
 
         System.out.print("A diagonal principal tem os seguintes elementos: ");
         for(int i = 0 ; i < matrix.length ; i++){
-            System.out.print(matrix[i][i]);
+            System.out.print(matrix[i][i] + " ");
         }
+
+
+        
+        
+        List <Integer> numberNegatives = new java.util.ArrayList<>();
+
+        for(int i = 0 ; i < matrix.length ; i++){
+            for (int j = 0 ; j < matrix[i].length; j++ ){
+               if (matrix[i][j] < 0) {
+                 numberNegatives.add(matrix[i][j]);
+               }
+            }
+        }
+
+        System.out.println("Os numeros negativos são estes: ");
+        System.out.print(numberNegatives);
+
+
+
+        sc.close();
     }
 }
